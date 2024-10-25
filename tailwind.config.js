242cda93a9fc66413c378f15eaf3958c57d139b2
LucasGdBS <lgbs@cesar.school>
2024-10-25 18:32:11 -0300

    adicionando cores para o estilo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'black-night-rider': '#171717',
+        'black-eclipse': '#212121',
+        'white-whisper': '#ECECEC',
+      }
+    },
   },
   plugins: [],
 };
