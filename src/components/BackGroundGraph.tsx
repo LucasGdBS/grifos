1d2948ad3658d0c31e101afedf6cf8bafb220509
LucasGdBS <lgbs@cesar.school>
2024-10-25 22:45:02 -0300

    adicionando Vertices pre-definidos no grafo

--- src/components/BackGroundGraph.tsx
+++ src/components/BackGroundGraph.tsx
@@ -1,11 +1,47 @@
-import { ReactFlow, Background } from "@xyflow/react";
+import {
+  ReactFlow,
+  Background,
+  Node,
+  useEdgesState,
+  useNodesState,
+  ConnectionMode,
+} from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
+import Vertice from "./Vertice";
+
+const NODE_TYPES = {
+  vertice: Vertice,
+};
+
+const INITIAL_NODES = [
+  {
+    id: "1",
+    type: "vertice",
+    data: { label: "A" },
+    position: { x: 200, y: 400 },
+  },
+  {
+    id: "2",
+    type: "vertice",
+    data: { label: "B" },
+    position: { x: 400, y: 400 },
+  },
+] satisfies Node[];
 
 export default function BackgroundGraph() {
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [nodes, setNodes, onNodesChange] = useNodesState(INITIAL_NODES);
+
   return (
     <div className="w-3/4 h-3/4 border border-white-whisper rounded-2xl">
-      <ReactFlow>
-        <Background/>
+      <ReactFlow
+        nodeTypes={NODE_TYPES}
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        connectionMode={ConnectionMode.Loose}
+      >
+        <Background />
       </ReactFlow>
     </div>
   );
---
+++ src/components/Vertice.tsx
@@ -0,0 +1,5 @@
+import { NodeProps } from "@xyflow/react";
+
+export default function Vertice(props: NodeProps) {
+  return <div className="bg-violet-500 rounded-full w-[150px] h-[150px]"></div>;
+}
