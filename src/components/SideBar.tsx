f9ba76ec655d78b7a2e2a96b220b5b9e6db16466
LucasGdBS <lgbs@cesar.school>
2024-11-01 23:25:35 -0300

    ajuste no estilo da navbar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -34,8 +34,6 @@ export default function SideBar({
   const [ativo, setAtivo] = useState<boolean>(false);
   const [, setVerticeEscolhido] = useState<string>("");
 
-
-
   const handleVerticeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setVerticeEscolhido(e.target.value);
     setNoEscolhido(e.target.value);
@@ -53,7 +51,6 @@ export default function SideBar({
     setToNode(e.target.value);
   };
 
-
   const handleToggle = (newValue: boolean) => {
     // Atualiza o estado do grafo direcionado
     setAtivo(newValue);
@@ -69,69 +66,80 @@ export default function SideBar({
   // TODO: Implementar função para transformar o texto em grafo
 
   return (
-      <aside className="w-64 bg-black-night-rider p-5 space-y-6">
-        <h1 className="text-2xl text-center">GRIFO</h1>
+    <aside className="w-80 bg-black-night-rider p-5 space-y-6">
+      <h1 className="text-2xl text-center">GRIFO</h1>
 
-        {/* Botão para alternar entre grafo direcionado e grafo não direcionado*/}
-        <div className="flex flex-col justify-center w-full items-center">
-          {ativo ? (
-              <h2 className="text-xl">Direcionado</h2>
-          ) : (
-              <h2 className="text-xl">Não direcionado</h2>
-          )}
-          <Interruptor onToggle={handleToggle}/>
-        </div>
+      {/* Botão para alternar entre grafo direcionado e grafo não direcionado*/}
+      <div className="flex flex-col justify-center w-full items-center">
+        {ativo ? (
+          <h2 className="text-xl">Direcionado</h2>
+        ) : (
+          <h2 className="text-xl">Não direcionado</h2>
+        )}
+        <Interruptor onToggle={handleToggle} />
+      </div>
 
-        <textarea
-            className="w-full h-32 p-2 text-gray-800 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            placeholder="Digite a string do grafo aqui..."
-            value={inputText}
-            onChange={handleInputChange}
-        ></textarea>
+      <textarea
+        className="w-full h-32 p-2 text-gray-800 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        placeholder="Digite a string do grafo aqui..."
+        value={inputText}
+        onChange={handleInputChange}
+      ></textarea>
 
-        <div className="justify-between flex">
-          <h2 className="text-xl">Ordem: {ordem}</h2>
-          <h2 className="text-xl">Tamanho: {tamanho}</h2>
-        </div>
+      <div className="justify-between flex">
+        <h2 className="text-xl">Ordem: {ordem}</h2>
+        <h2 className="text-xl">Tamanho: {tamanho}</h2>
+      </div>
 
-        <div className="text-gray-800 flex flex-col space-y-4">
-          <input
-              type="text"
-              placeholder="Vértice de origem"
-              onChange={handleFromNodeChange}
-              className="p-2 rounded-md"
-          />
-          <input
-              type="text"
-              placeholder="Vértice de destino"
-              onChange={handleToNodeChange}
-              className="p-2 rounded-md"
-          />
-          <h2 className="text-white text-lg">
-            Adjacente? {areNodesAdjacent ? "Sim" : "Não"}
-          </h2>
-        </div>
+      <div className="text-gray-800 flex flex-col space-y-4">
+        <input
+          type="text"
+          placeholder="Vértice de origem"
+          onChange={handleFromNodeChange}
+          className="p-2 rounded-md"
+        />
+        <input
+          type="text"
+          placeholder="Vértice de destino"
+          onChange={handleToNodeChange}
+          className="p-2 rounded-md"
+        />
+        {areNodesAdjacent ? (
+          <h2 className="text-green-500 text-lg">Vértices são adjacentes</h2>
+        ) : (
+          <h2 className="text-red-600 text-lg">Vértices não são adjacentes</h2>
+        )}
+      </div>
 
-        <div className="text-gray-800 flex flex-col space-y-8">
-          <input type="text" placeholder={"Escolha um vértice"} onChange={handleVerticeChange}/>
-          {ativo ? (
-              <div className="flex flex-col space-y-4 text-lg text-white">
-                <div className="flex flex-col">
-                  <h2>Vértices de Entrada : </h2>
-                  <h2>{adjacenteEntrada}</h2>
-                </div>
-                <div className="flex flex-col">
-                  <h2>Vértices de Saída : </h2>
-                  <h2>{adjacenteSaida}</h2>
-                </div>
-              </div>
-          ) : (
-              <h2 className="text-white text-xl"> Vértices Adjacentes {`${adjacenteEntrada} ${adjacenteSaida}`}</h2>
-          )}
-
-        </div>
-      </aside>
-
-
+      <div className="text-gray-800 flex flex-col space-y-8">
+        <input
+          type="text"
+          placeholder={"Escolha um vértice"}
+          onChange={handleVerticeChange}
+          className="p-2 rounded-md"
+        />
+        {ativo ? (
+          <div className="flex flex-col space-y-4 text-lg text-white">
+            <div className="flex flex-col">
+              <h2>Vértices de Entrada : </h2>
+              <h2>{adjacenteEntrada.join(" ")}</h2>
+            </div>
+            <div className="flex flex-col">
+              <h2>Vértices de Saída : </h2>
+              <h2>{adjacenteSaida.join(" ")}</h2>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <h2 className="text-white text-xl">Vértices Adjacentes: </h2>
+            <h2 className="text-white text-xl">
+              {Array.from(
+                new Set([...adjacenteEntrada, ...adjacenteSaida])
+              ).join(" ")}
+            </h2>
+          </div>
+        )}
+      </div>
+    </aside>
   );
 }
